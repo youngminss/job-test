37211fcdc92e908f092095a48f0f0338712156d9
Wie Youngmin <50790145+youngminss@users.noreply.github.com>
2021-12-29 23:23:08 +0900

    Chore: Build production separate setting
    + webpack-merge 패키지 설치
    + webpack.common.js : prod & dev 두 환경에서 공통적으로 적용될 사항 기재
    + webpack.dev.js : development 환경에서만 적용될 사항 기재
    + webpack.prod.js : production 환경에서만 적용될 사항 기재
    + webpack.config.js : script 에서 env 옵션이 무엇인지에 따라 commonConfig와 merge한 번들링 파일을 생성

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ import "./style.css";
 const App = () => {
   return (
     <div>
-      <h1>React - TS Webpack Starter Template !!</h1>
+      <h1>
+        React - TS Webpack Starter Template !! - {process.env.NODE_ENV} {process.env.name}
+      </h1>
       <img src={img} alt="Webpack img resource test" width="300" height="200" />
       <img src={LOGO} alt="Webpack svg resource test" width="300" height="200" />
     </div>
